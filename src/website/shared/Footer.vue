1c69049f6b4a1c93a136d7a7ec390792ab980fc1
Mahmoud <Mahmoud@Live.com>
2020-08-14 15:01:07 +0200

    Sponsor footer

--- src/website/shared/Footer.vue
+++ src/website/shared/Footer.vue
@@ -6,7 +6,7 @@
     ]"
   >
     <div
-      v-for="(sponsor, index) in sponsors.slice(0, 5)"
+      v-for="(sponsor, index) in sponsors"
       :key="index"
       @click="redirectTo(sponsor.link)"
       role="button"
