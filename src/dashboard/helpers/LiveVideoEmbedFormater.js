37098d38c3d5d27557b49f501112a910fbc65684
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-08-27 15:35:23 +0200

    refactor get live video and chat

--- src/dashboard/helpers/LiveVideoEmbedFormater.js
+++ src/dashboard/helpers/LiveVideoEmbedFormater.js
@@ -4,22 +4,16 @@ const liveVideoEmbedFormatter = (url) => {
         return `https://www.youtube.com/embed/${vidId}`
     }
 
-    if (url.includes("youtube") && url.includes("embed"))
-        return url
-
     if (url.includes("twitch")) {
         const items = url.split("/")
         const channel = items[items.length - 1]
-        console.log('====================================');
-        console.log(`https://player.twitch.tv/?channel=${channel}&parent=${window.location.hostname}`);
-        console.log('====================================');
         return `https://player.twitch.tv/?channel=${channel}&parent=${window.location.hostname}`
     }
     return url
 }
 
 const liveVideoChatEmbedFormatter = (url) => {
-    if (url.includes("youtube") && url.includes("embed")) {
+    if (url.includes("youtube")) {
         const vidId = url.split("=")[1]
         return `https://www.youtube.com/live_chat?v=${vidId}&embed_domain=${window.location.hostname}`
     }
@@ -27,9 +21,6 @@ const liveVideoChatEmbedFormatter = (url) => {
     if (url.includes("twitch")) {
         const items = url.split("/")
         const channel = items[items.length - 1]
-        console.log('====================================');
-        console.log(`https://www.twitch.tv/embed/${channel}/chat?parent=${window.location.hostname}`);
-        console.log('====================================');
         return `https://www.twitch.tv/embed/${channel}/chat?parent=${window.location.hostname}`
     }
     return url
