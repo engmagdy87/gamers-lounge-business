a28665df4e400a6719e421741dfbf512a507b3fe
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-11-11 01:21:43 +0200

    add google map

--- src/website/containers/Contact.vue
+++ src/website/containers/Contact.vue
@@ -22,6 +22,16 @@
       </template>
     </Hero>
     <FormContainer />
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.113134868015!2d31.427575215691927!3d30.00490768189666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583cc723219aad%3A0x3cab9244af63e06e!2sGamers%20Lounge!5e0!3m2!1sen!2seg!4v1605050261157!5m2!1sen!2seg"
+      width="100%"
+      height="600"
+      frameborder="0"
+      style="border:0;"
+      allowfullscreen=""
+      aria-hidden="false"
+      tabindex="0"
+    ></iframe>
   </div>
 </template>
 
