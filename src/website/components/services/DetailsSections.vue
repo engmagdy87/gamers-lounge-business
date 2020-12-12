217b7f148a97e376900bb89e09f963a66fdc554e
Mostafa <mostafaelkhmisi@hotmail.com>
2020-12-12 18:21:09 +0200

    services finished

--- src/website/components/services/DetailsSections.vue
+++ src/website/components/services/DetailsSections.vue
@@ -1,46 +1,6 @@
 <template>
 <div>
       <div v-for="service in sectionData" :key="service.id">
-        <div class="section-container sections-temp4">
-        <div class="section-container__template row m-0"
-        >
-            <h1 class="col-12 p-0 m-0 section-container__title pb-5" > {{service.title}} </h1>
-            
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 section-container__media">
-                <img v-if="service.img_content_first"
-                  v-for="(img, i) in service.img_content_first"
-                  :key="i" 
-                  :src="img.url" 
-                >
-                <video v-if="service.vid_content_first"
-                  v-for="(video, i) in service.vid_content_first"
-                  :key="i" :src="video.url"
-                  controls
-                  > </video>
-            </div>
-
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 pr-3">
-                <p v-html="service.description_first"></p>
-            </div>
-
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 pr-3">
-                <p v-html="service.description_second"></p>
-            </div>
-
-            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 section-container__media">
-                <img v-if="service.img_content_second"
-                  v-for="(img, i) in service.img_content_second"
-                  :key="i" 
-                  :src="img.url" 
-                >
-                <video v-if="service.vid_content_second"
-                  v-for="(video, i) in service.vid_content_second"
-                  :key="i" :src="video.url"
-                  controls
-                  > </video>
-            </div>
-        </div>
-        </div>
 
         <Template1 :data="service" v-if="service.type == 'TEMPLATE_1'"/>
 
