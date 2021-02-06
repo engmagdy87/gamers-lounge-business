ed767c47a920a30fb320fd52bda2116597ebb442
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2021-02-06 21:07:48 +0200

    build locally and remove dist from gitignore

---
+++ dist/precache-manifest.cdcf2fbe1ee45255a931688decaef824.js
@@ -0,0 +1,534 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bff57b9ac11666d06a4",
+    "url": "/css/About.1366fbfe.css"
+  },
+  {
+    "revision": "0bff57b9ac11666d06a4",
+    "url": "/js/About.afcd7d89.js"
+  },
+  {
+    "revision": "ab455f432b22457ae7d5",
+    "url": "/css/AboutColumnsAddEdit.99d946aa.css"
+  },
+  {
+    "revision": "ab455f432b22457ae7d5",
+    "url": "/js/AboutColumnsAddEdit.a1c08b2f.js"
+  },
+  {
+    "revision": "8e1fa27d7a253ca3302a",
+    "url": "/css/AboutColumnsAddEdit~ServiceColumnsAddEdit~WorkColumnsAddEdit.bcf4d0b7.css"
+  },
+  {
+    "revision": "8e1fa27d7a253ca3302a",
+    "url": "/js/AboutColumnsAddEdit~ServiceColumnsAddEdit~WorkColumnsAddEdit.a7a0bdfe.js"
+  },
+  {
+    "revision": "dfe16cf07e94668dfb89",
+    "url": "/css/AboutColumnsList.bac9a0b8.css"
+  },
+  {
+    "revision": "dfe16cf07e94668dfb89",
+    "url": "/js/AboutColumnsList.35bb5476.js"
+  },
+  {
+    "revision": "62a64f7f8d06c6b6b8e6",
+    "url": "/css/AboutColumnsList~AboutRowsList~AboutSectionsList~DepartmentsList~JobApplicantsList~JobsList~Overview~afea21d1.b4bddc10.css"
+  },
+  {
+    "revision": "62a64f7f8d06c6b6b8e6",
+    "url": "/js/AboutColumnsList~AboutRowsList~AboutSectionsList~DepartmentsList~JobApplicantsList~JobsList~Overview~afea21d1.c0eec812.js"
+  },
+  {
+    "revision": "d64f109dc0809ba5365a",
+    "url": "/js/AboutRowsAddEdit.6444d9c8.js"
+  },
+  {
+    "revision": "c4559b42702e711f533e",
+    "url": "/css/AboutRowsList.666802d3.css"
+  },
+  {
+    "revision": "c4559b42702e711f533e",
+    "url": "/js/AboutRowsList.6133ad26.js"
+  },
+  {
+    "revision": "2e84b1c8997ca0c59211",
+    "url": "/js/AboutSectionsAddEdit.0bc790a7.js"
+  },
+  {
+    "revision": "aaa75a37b6bdbf410b04",
+    "url": "/css/AboutSectionsList.527d9a6c.css"
+  },
+  {
+    "revision": "aaa75a37b6bdbf410b04",
+    "url": "/js/AboutSectionsList.47dea7cd.js"
+  },
+  {
+    "revision": "05d6232c2760efc92897",
+    "url": "/css/About~OurWorkDetails~ServicesDetails.f4be1ecc.css"
+  },
+  {
+    "revision": "05d6232c2760efc92897",
+    "url": "/js/About~OurWorkDetails~ServicesDetails.5cb5d185.js"
+  },
+  {
+    "revision": "10491ee928e6d530dec8",
+    "url": "/css/Contact.6340a88c.css"
+  },
+  {
+    "revision": "10491ee928e6d530dec8",
+    "url": "/js/Contact.78094322.js"
+  },
+  {
+    "revision": "87e3a31edc733be14365",
+    "url": "/css/ContactAddEdit.96003a7b.css"
+  },
+  {
+    "revision": "87e3a31edc733be14365",
+    "url": "/js/ContactAddEdit.ed9ac23d.js"
+  },
+  {
+    "revision": "3415c87c8c8ca8f6f3d5",
+    "url": "/css/DashboardLayout.90d5bc15.css"
+  },
+  {
+    "revision": "3415c87c8c8ca8f6f3d5",
+    "url": "/js/DashboardLayout.f4b22ed1.js"
+  },
+  {
+    "revision": "067e891490bb52e1ea22",
+    "url": "/js/DepartmentsAdd.e76ac419.js"
+  },
+  {
+    "revision": "68813eac0cc1a959ec10",
+    "url": "/css/DepartmentsList.404898b2.css"
+  },
+  {
+    "revision": "68813eac0cc1a959ec10",
+    "url": "/js/DepartmentsList.8ad2e1a3.js"
+  },
+  {
+    "revision": "5e410b2a8d1c6a7eac8a",
+    "url": "/css/Home.8a7c44d0.css"
+  },
+  {
+    "revision": "5e410b2a8d1c6a7eac8a",
+    "url": "/js/Home.322ebff5.js"
+  },
+  {
+    "revision": "e0ec7f126732cd93b4c9",
+    "url": "/js/Icons.06440426.js"
+  },
+  {
+    "revision": "6fa4f213354de294589c",
+    "url": "/css/JobApplicantsList.f7de9e1b.css"
+  },
+  {
+    "revision": "6fa4f213354de294589c",
+    "url": "/js/JobApplicantsList.28b7cb87.js"
+  },
+  {
+    "revision": "3e8ca819c580d5d3d5ce",
+    "url": "/css/JobDetails.ad219e83.css"
+  },
+  {
+    "revision": "3e8ca819c580d5d3d5ce",
+    "url": "/js/JobDetails.d1fbb65e.js"
+  },
+  {
+    "revision": "d937f46f77ec461b2703",
+    "url": "/css/Jobs.112e77d7.css"
+  },
+  {
+    "revision": "d937f46f77ec461b2703",
+    "url": "/js/Jobs.d46b6615.js"
+  },
+  {
+    "revision": "d81295204d2ad4d776d7",
+    "url": "/js/JobsAdd.f3467816.js"
+  },
+  {
+    "revision": "23d0f8eed1a82ea1005c",
+    "url": "/css/JobsList.45d10524.css"
+  },
+  {
+    "revision": "23d0f8eed1a82ea1005c",
+    "url": "/js/JobsList.9ef28a90.js"
+  },
+  {
+    "revision": "75f0c4f3c97bf6ac5520",
+    "url": "/css/Login.42cba0c4.css"
+  },
+  {
+    "revision": "75f0c4f3c97bf6ac5520",
+    "url": "/js/Login.01179aad.js"
+  },
+  {
+    "revision": "b54fa2f3d2ca8800fc85",
+    "url": "/js/NotFound.ad4663a7.js"
+  },
+  {
+    "revision": "d567d8caca78a9ffdfbb",
+    "url": "/js/Notifications.9859d2a8.js"
+  },
+  {
+    "revision": "5e3bb7f95c3ab50effd2",
+    "url": "/css/OurWorkDetails.b42c1d24.css"
+  },
+  {
+    "revision": "5e3bb7f95c3ab50effd2",
+    "url": "/js/OurWorkDetails.239b9249.js"
+  },
+  {
+    "revision": "94b824fdda9738f89ee7",
+    "url": "/css/OurWorkDetails~ServicesDetails.6c9f77fe.css"
+  },
+  {
+    "revision": "94b824fdda9738f89ee7",
+    "url": "/js/OurWorkDetails~ServicesDetails.349a074a.js"
+  },
+  {
+    "revision": "df4cb54f9493edce41df",
+    "url": "/css/OurWorks.a96b4fb3.css"
+  },
+  {
+    "revision": "df4cb54f9493edce41df",
+    "url": "/js/OurWorks.a1b876f7.js"
+  },
+  {
+    "revision": "ee66678b77e30ee16161",
+    "url": "/js/Overview.601127ac.js"
+  },
+  {
+    "revision": "c8b629cac8ec8c748711",
+    "url": "/css/ServiceColumnsAddEdit.99d946aa.css"
+  },
+  {
+    "revision": "c8b629cac8ec8c748711",
+    "url": "/js/ServiceColumnsAddEdit.d2698c6b.js"
+  },
+  {
+    "revision": "c95495bc45c7bb475581",
+    "url": "/css/ServiceColumnsList.ef7608c9.css"
+  },
+  {
+    "revision": "c95495bc45c7bb475581",
+    "url": "/js/ServiceColumnsList.03c0f354.js"
+  },
+  {
+    "revision": "26934b1e034e0b683732",
+    "url": "/js/ServiceRowsAddEdit.fafd63e6.js"
+  },
+  {
+    "revision": "1c738b77748b35cc04cc",
+    "url": "/css/ServiceRowsList.30e4ea0c.css"
+  },
+  {
+    "revision": "1c738b77748b35cc04cc",
+    "url": "/js/ServiceRowsList.c0bebdcc.js"
+  },
+  {
+    "revision": "f45abd409dcb242b8db3",
+    "url": "/js/ServiceSectionsAddEdit.7e6009b8.js"
+  },
+  {
+    "revision": "85bf904f3fb5f97d0c45",
+    "url": "/css/ServiceSectionsList.92516e85.css"
+  },
+  {
+    "revision": "85bf904f3fb5f97d0c45",
+    "url": "/js/ServiceSectionsList.d7d3dfbc.js"
+  },
+  {
+    "revision": "6ed48306a3f1c6e25583",
+    "url": "/css/Services.371a685c.css"
+  },
+  {
+    "revision": "6ed48306a3f1c6e25583",
+    "url": "/js/Services.48eabcc1.js"
+  },
+  {
+    "revision": "2043020fedae53031d8a",
+    "url": "/css/ServicesAddEdit.dea2308b.css"
+  },
+  {
+    "revision": "2043020fedae53031d8a",
+    "url": "/js/ServicesAddEdit.8236c9c8.js"
+  },
+  {
+    "revision": "8f21f3f4f6d0e8624a19",
+    "url": "/css/ServicesDetails.c26d6306.css"
+  },
+  {
+    "revision": "8f21f3f4f6d0e8624a19",
+    "url": "/js/ServicesDetails.234fffbe.js"
+  },
+  {
+    "revision": "7883edd3af8ddb7e34e8",
+    "url": "/css/ServicesList.b4d24517.css"
+  },
+  {
+    "revision": "7883edd3af8ddb7e34e8",
+    "url": "/js/ServicesList.307d53d1.js"
+  },
+  {
+    "revision": "7ccf633552068895925f",
+    "url": "/css/SponsorsAddEdit.ce41dbab.css"
+  },
+  {
+    "revision": "7ccf633552068895925f",
+    "url": "/js/SponsorsAddEdit.c2c91963.js"
+  },
+  {
+    "revision": "9cc779f82241c07209a9",
+    "url": "/css/SponsorsList.d5d69efc.css"
+  },
+  {
+    "revision": "9cc779f82241c07209a9",
+    "url": "/js/SponsorsList.4dc6325d.js"
+  },
+  {
+    "revision": "02ee50c02034c47cff49",
+    "url": "/css/TeamAddEdit.ce41dbab.css"
+  },
+  {
+    "revision": "02ee50c02034c47cff49",
+    "url": "/js/TeamAddEdit.85975fac.js"
+  },
+  {
+    "revision": "523a0c9e3d952bbfc6d8",
+    "url": "/css/TeamList.f5298a43.css"
+  },
+  {
+    "revision": "523a0c9e3d952bbfc6d8",
+    "url": "/js/TeamList.d701c957.js"
+  },
+  {
+    "revision": "4083bef4722e864c20ec",
+    "url": "/css/WorkColumnsAddEdit.99d946aa.css"
+  },
+  {
+    "revision": "4083bef4722e864c20ec",
+    "url": "/js/WorkColumnsAddEdit.b0afaa27.js"
+  },
+  {
+    "revision": "6dc4d86d70ff8d5b56d6",
+    "url": "/css/WorkColumnsList.54e51820.css"
+  },
+  {
+    "revision": "6dc4d86d70ff8d5b56d6",
+    "url": "/js/WorkColumnsList.7acfcf6d.js"
+  },
+  {
+    "revision": "35e39111c0837d81ed43",
+    "url": "/js/WorkRowsAddEdit.d17afa55.js"
+  },
+  {
+    "revision": "e42fe53a6705008510b0",
+    "url": "/css/WorkRowsList.f10e1ade.css"
+  },
+  {
+    "revision": "e42fe53a6705008510b0",
+    "url": "/js/WorkRowsList.30d819b4.js"
+  },
+  {
+    "revision": "004b6e8257240d893bd3",
+    "url": "/js/WorkSectionsAddEdit.07f9144c.js"
+  },
+  {
+    "revision": "a4effdeb29852888c891",
+    "url": "/css/WorkSectionsList.5d21d26f.css"
+  },
+  {
+    "revision": "a4effdeb29852888c891",
+    "url": "/js/WorkSectionsList.1ab741d6.js"
+  },
+  {
+    "revision": "caecaf73bc6d1523d6c8",
+    "url": "/css/WorksAddEdit.dea2308b.css"
+  },
+  {
+    "revision": "caecaf73bc6d1523d6c8",
+    "url": "/js/WorksAddEdit.e93f82ce.js"
+  },
+  {
+    "revision": "9862adfb2f91459df2a7",
+    "url": "/css/WorksList.728e94ee.css"
+  },
+  {
+    "revision": "9862adfb2f91459df2a7",
+    "url": "/js/WorksList.d3472f60.js"
+  },
+  {
+    "revision": "5aed92b93b0a685a41b7",
+    "url": "/css/app.8f0f0885.css"
+  },
+  {
+    "revision": "5aed92b93b0a685a41b7",
+    "url": "/js/app.b38c292f.js"
+  },
+  {
+    "revision": "e2d928018cb3ba2a805c",
+    "url": "/js/chunk-2d219ff9.0aab29fd.js"
+  },
+  {
+    "revision": "377bcfb6ee36986550f4",
+    "url": "/css/chunk-vendors.6f221b87.css"
+  },
+  {
+    "revision": "377bcfb6ee36986550f4",
+    "url": "/js/chunk-vendors.1715d145.js"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/fonts/Segoe-UI.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/fonts/Segoe-UI-Bold.65099f98.ttf"
+  },
+  {
+    "revision": "676a12075e14377f6f44a3e29b4d72af",
+    "url": "/fonts/Segoe-UI-Bold-Italic.676a1207.ttf"
+  },
+  {
+    "revision": "5eb716ee36ae3ea66de512bcb63af3d8",
+    "url": "/fonts/Segoe-UI-Italic.5eb716ee.ttf"
+  },
+  {
+    "revision": "273f1bf7e4259eb1965d7267b656ef72",
+    "url": "/img/pin-white.273f1bf7.svg"
+  },
+  {
+    "revision": "8787c6a53212c3033dc26fb3eb820305",
+    "url": "/img/prev.8787c6a5.svg"
+  },
+  {
+    "revision": "380584c351ab83cce612137bb67dfeb5",
+    "url": "/img/mail-white.380584c3.svg"
+  },
+  {
+    "revision": "7a2e9e64aec968dc81d8d9320a18ac0f",
+    "url": "/img/GL-logo.7a2e9e64.png"
+  },
+  {
+    "revision": "6bf694c12af83036f037e9825ba073c7",
+    "url": "/img/phone-white.6bf694c1.svg"
+  },
+  {
+    "revision": "973d1b853335d9eb78eaf40e4eb6272d",
+    "url": "/img/top-arrow.973d1b85.svg"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "c9e7debf1f052891316f621ed282ff5c",
+    "url": "/img/next.c9e7debf.svg"
+  },
+  {
+    "revision": "bb7d4343407d278c379b1fe97b5a8b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a2e9e64aec968dc81d8d9320a18ac0f",
+    "url": "/images/GL-logo.png"
+  },
+  {
+    "revision": "380584c351ab83cce612137bb67dfeb5",
+    "url": "/images/mail-white.svg"
+  },
+  {
+    "revision": "c9e7debf1f052891316f621ed282ff5c",
+    "url": "/images/next.svg"
+  },
+  {
+    "revision": "6bf694c12af83036f037e9825ba073c7",
+    "url": "/images/phone-white.svg"
+  },
+  {
+    "revision": "273f1bf7e4259eb1965d7267b656ef72",
+    "url": "/images/pin-white.svg"
+  },
+  {
+    "revision": "8787c6a53212c3033dc26fb3eb820305",
+    "url": "/images/prev.svg"
+  },
+  {
+    "revision": "973d1b853335d9eb78eaf40e4eb6272d",
+    "url": "/images/top-arrow.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.gitkeep"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "0d4ffe0df1a0d1e770c5138f158d94b2",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "5b47b416f6e4dbba80a1ca269adf7580",
+    "url": "/images/decrease_font.svg"
+  },
+  {
+    "revision": "a95be6e8a890f1897308ee3122a70aef",
+    "url": "/images/add.svg"
+  },
+  {
+    "revision": "752d64f551b54c06f4b7280d9f74d27c",
+    "url": "/images/delete.svg"
+  },
+  {
+    "revision": "533b554da9fa067f3e7e2a631cc83a80",
+    "url": "/images/download.svg"
+  },
+  {
+    "revision": "cd955919d1908c1f33233b0f8e3b05b2",
+    "url": "/images/edit.svg"
+  },
+  {
+    "revision": "658814864a666a3b4cab6cfdc1cf14e1",
+    "url": "/images/env.svg"
+  },
+  {
+    "revision": "80ab8c35fee0090540b1acde85807338",
+    "url": "/images/increase_font.svg"
+  },
+  {
+    "revision": "6ba855e58d163e941e34e5df303e01b8",
+    "url": "/images/pin.svg"
+  },
+  {
+    "revision": "0d5b244ec347b92c573030f4514a34f3",
+    "url": "/images/phone.svg"
+  },
+  {
+    "revision": "b92d3a8381ec141ff42c430874bca38c",
+    "url": "/images/hero.jpg"
+  },
+  {
+    "revision": "8ab7e03c6fd3405f3ebfd157b2b71240",
+    "url": "/images/visibility.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
